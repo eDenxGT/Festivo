284ba65def0ad2522aeb6003dcf37212ba0ab274
eDenxGT <edenxgt@email.com>
2025-09-19 09:50:32 +0530

    fix: added loading while registration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,38 @@
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./utils/ScrollToTop";
-import UnauthorizedPage from "./pages/common/UnauthorizedPage";
-import OrganizerRoutes from "./routes/OrganizerRoutes";
-import UserRoutes from "./routes/UserRoutes";
 import LandingPage from "./pages/common/LandingPage";
 import { NoAuthRoute } from "./utils/protected/PublicRoute";
-import NotFoundPage from "./pages/common/NotFound";
+
+// Lazy imports
+const OrganizerRoutes = React.lazy(() => import("./routes/OrganizerRoutes"));
+const UserRoutes = React.lazy(() => import("./routes/UserRoutes"));
+const UnauthorizedPage = React.lazy(
+	() => import("./pages/common/UnauthorizedPage")
+);
+const NotFoundPage = React.lazy(() => import("./pages/common/NotFound"));
 
 function App() {
 	return (
 		<Router>
 			<ScrollToTop />
-			<Routes>
-				<Route
-					path="/"
-					element={<NoAuthRoute element={<LandingPage />} />}
-				/>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route
+						path="/"
+						element={<NoAuthRoute element={<LandingPage />} />}
+					/>
 
-				<Route path="/user/*" element={<UserRoutes />} />
+					<Route path="/user/*" element={<UserRoutes />} />
 
-				<Route path="/org/*" element={<OrganizerRoutes />} />
-				<Route path="/unauthorized" element={<UnauthorizedPage />} />
-				<Route path="*" element={<NotFoundPage />} />
-			</Routes>
+					<Route path="/org/*" element={<OrganizerRoutes />} />
+					<Route
+						path="/unauthorized"
+						element={<UnauthorizedPage />}
+					/>
+					<Route path="*" element={<NotFoundPage />} />
+				</Routes>
+			</Suspense>
 		</Router>
 	);
 }
